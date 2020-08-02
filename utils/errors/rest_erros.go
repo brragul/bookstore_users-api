d618d31869940b6ff6b912c3df2e8ced86f305be
Balakrishnan <balakrir@rose02ym02qjgh7.fios-router.home>
2020-08-02 17:58:59 -0400

    dao structure

---
+++ utils/errors/rest_erros.go
@@ -0,0 +1,25 @@
+package errors
+
+import "net/http"
+
+type RestErr struct {
+	Message string `json:"message"`
+	Code int `json:"code"`
+	Error string `json:"error"`
+}
+
+func NewBadRequestError(message string)*RestErr{
+	return &RestErr{
+		Message: message,
+		Code: http.StatusBadRequest,
+		Error: "bad_request",
+	}
+}
+
+func NewNotFound(message string)*RestErr{
+	return &RestErr{
+		Message: message,
+		Code: http.StatusNotFound,
+		Error: "bad_request",
+	}
+}
